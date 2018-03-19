8faca7a74078bfbe2d855e7160ae70f5aff1d3c7
Taddes Korris <taddes.korris@gmail.com>
2018-03-18 22:23:22 -0400

    first commit

---
+++ liri.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+// `my-tweets`
+//`spotify-this-song`
+//`movie-this`
+// `do-what-it-says`
