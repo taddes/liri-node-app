cbe854ecf4f84a37f2096c614f7165f100fa5274
taddes <taddes.korris@gmail.com>
2018-03-20 21:47:57 -0400

    all npm packages included, initialized

--- liri.js
+++ liri.js
@@ -1,4 +1,18 @@
+//DotENV require and config
 require("dotenv").config();
+
+//OMDB API KEY
+//&apikey=4645abe9
+
+//Request configure and save to variable request
+var request = require('request');
+
+//Spotify-API Module
+var Spotify = require('node-spotify-api');
+
+
+//Twitter npm Module
+var Twitter = require('twitter');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
