25e484dccd150711a3da75b2a8cabd129ba996e4
Taddes Korris <taddes.korris@gmail.com>
2018-03-22 23:57:55 -0400

    updates

--- liri.js
+++ liri.js
@@ -11,7 +11,6 @@ var Spotify = require('node-spotify-api');
 //Twitter npm Module
 var Twitter = require('twitter');
 
-
 //assign movieTitle var to argv[2]
 var action = process.argv[2];
 var value = process.argv[3];
@@ -43,19 +42,25 @@ switch (action) {
 
 function movieThis() {
 
+// if (process.argv[3] !="" && process.argv[3] != null) {
+//     var value = process.argv[3].trim();
+// } else  {
+//     value = ("Mr.+Nobody");
+// }
 
- var value = process.argv.slice(3).join("+");
- console.log("value: " + value);
-
+var value = process.argv.slice(3).join("+");
     //set up query to OMDB API
     var queryUrl = "http://www.omdbapi.com/?t=" + value + "&apikey=4645abe9";
 
-    console.log(queryUrl);
+    // console.log(queryUrl);
 
     request(queryUrl, function (error, response, body) {
+  
 
         if (!error && response.statusCode === 200) {
 
+            console.log("Here is the movie info you requested about " + value.toUpperCase() + ", Taddes, comliments of me, Liri: ");
+            console.log("========================MOVIES========================");
             console.log("Title: " + JSON.parse(body).Title);
             console.log("Year of Release: " + JSON.parse(body).Year);
             console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
@@ -64,7 +69,10 @@ function movieThis() {
             console.log("Language: " + JSON.parse(body).Language);
             console.log("Plot: " + JSON.parse(body).Plot);
             console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("========================MOVIES========================");
         }
+
+         
     })
 
 } //end of movieThis function
@@ -75,18 +83,31 @@ function spotifyThisSong() {
     // console.log("value: " + value);
 
     var spotify = new Spotify(keys.spotify);
-    // {
-    //     id: process.env.SPOTIFY_ID,
-    //     secret: process.env.SPOTIFY_SECRET
-    // });
+    
+    var value = process.argv.slice(3).join(" ");
 
-    spotify.search({ type: 'track', query: value, limit: 1 }, function (err, data) {
+    spotify.search({ type: 'track', query: value, limit: 1}, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        var songInfo = data.tracks.items[0];
-        console.log(songInfo.artists[0].name);
-        console.log(songInfo.album.name);
+
+        
+        console.log(value);
+
+        //declaration to break down code needed to access pathway
+        var songInfo = data.tracks.items[1];
+        // console.log(songInfo);
+        console.log("Here is the song info on " + value.toUpperCase() + " that you requested, Taddes, comliments of me, Liri: ");
+        console.log("========================SPOTIFY========================");
+        console.log("Artist(s): " + songInfo.artists[0].name);
+        console.log("Song Name: " +songInfo.album.name);
+        console.log("Album: " +songInfo.album.name);
+        if( songInfo.preview_url === null) {
+            console.log("No song preview link available");
+        } else {
+            console.log("Album Preview URL: " +songInfo.preview_url);
+        }
+        console.log("========================SPOTIFY========================");
     });
 
 
@@ -99,19 +120,30 @@ function myTweets() {
       var params = {screen_name: 'taddes_k', count: 20};
       client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
-          console.log(tweets);
+        //   console.log(tweets);
         }
-        for (var i = 0; i <tweets.length ; i++) {
-            console.log(tweets[i].text);
+        console.log("Here are your Tweets, Taddes, comliments of me, Liri: ");
+        console.log("========================Tweets========================");
+        for (var i = 0; i < tweets.length ; i++) {
+            console.log("Tweet No." + i + ": " +  tweets[i].text);
         }
+        console.log("========================Tweets========================");
       });
 }
 
-//output to log.txt function BONUS
+
 
 
 var fs = require("fs");
 
+function doWhatItSays(){
+	fs.readFile("random.txt", "utf8", function(error, data){
+		console.log(data);
+	})
+
+}
+
+//output to log.txt function BONUS
 function appendToLog() {
 
 
